f30aa1270323e4f9803040a8a6bb00f99db26e27
ratth3cat <ratth3cat@gmail.com>
2025-10-26 23:01:00 -0600

    self port

---
+++ Homework9/Evening_okra_2025_10_27_04_58_05/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+  text('hi', 50, 50);
+}
+
+function draw() {
+  background(220);
+  rect (80,70,100,50);
+  circle (260,100,70);
+  triangle (150, 250, 200, 120, 200, 250);
+  line (100, 300, 275, 300);
+  line (175, 100, 225, 100);
+  line (225, 65, 275, 65);
+  point (315, 215);
+  point (250, 215);
+  point (282, 155) ;
+    textSize(32);
+    text('I AM', 10, 30);
+    textSize(15)
+    text('Dan Kallstrom', 300, 370);
+}
